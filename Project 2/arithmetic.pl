f8344fac8aec83536d095a3e61e303be05c81ffe
David Shin <shin.david93@gmail.com>
2021-03-17 14:46:28 -0700

    re-organized files

---
+++ Project 2/arithmetic.pl
@@ -0,0 +1,20 @@
+predecessor(X,Y) :- X=s(Y), X\=z.
+successor(X,Y) :- Y=s(X).
+
+numeral(z, 0).
+numeral(s(X),Y) :- numeral(X,Z), Y is +(Z,1).
+
+addition(z,Y,Y).
+addition(s(X),Y,s(Z)) :- addition(X,Y,Z).
+
+subtraction(X,z,X).
+subtraction(s(X),s(Y),Z) :- subtraction(X,Y,Z).
+
+multiplication(X,Y,Z) :- numeral(X,A), numeral(Y,B), T is A*B, numeral(Z,T), !.
+
+factorial(z,s(z)).
+factorial(X,Z) :- predecessor(X,P), factorial(P,Y), multiplication(X,Y,Z). 
+
+equal(X,Y) :- X=Y.
+greater(X,Y) :- X>Y.
+less(X,Y) :- X<Y.
--- Project 2/travel.pl
+++ Project 2/travel.pl
@@ -1,5 +1,3 @@
-Part A:
-
 onestep(X,Y, byCar, C) :- byCar(X,Y,C).
 onestep(X,Y, byTrain, C) :- byTrain(X,Y,C).
 onestep(X,Y, byPlane, C) :- byPlane(X,Y,C).
@@ -22,26 +20,3 @@ byPlane(paris,losAngeles,780).
 byPlane(bangkok,auckland,840).
 byPlane(singapore,auckland,590).
 byPlane(losAngeles,auckland,820).
-
-Part B:
-
-predecessor(X,Y) :- X=s(Y), X\=z.
-successor(X,Y) :- Y=s(X).
-
-numeral(z, 0).
-numeral(s(X),Y) :- numeral(X,Z), Y is +(Z,1).
-
-addition(z,Y,Y).
-addition(s(X),Y,s(Z)) :- addition(X,Y,Z).
-
-subtraction(X,z,X).
-subtraction(s(X),s(Y),Z) :- subtraction(X,Y,Z).
-
-multiplication(X,Y,Z) :- numeral(X,A), numeral(Y,B), T is A*B, numeral(Z,T), !.
-
-factorial(z,s(z)).
-factorial(X,Z) :- predecessor(X,P), factorial(P,Y), multiplication(X,Y,Z). 
-
-equal(X,Y) :- X=Y.
-greater(X,Y) :- X>Y.
-less(X,Y) :- X<Y.
